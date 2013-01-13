9cfc1e74be2c461d719c9fbef2b698cc86279af1
Travis Abbott <tabbott@genome.wustl.edu>
2013-01-13 00:03:33 -0600

    update redis host

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-export FLOW_REDIS_URL=linus129
+export FLOW_REDIS_URL=blade14-2-5
 export AMQP_URL=amqp://guest:guest@vmpool82:5672/workflow
 export PATH=`pwd`/bin:$PATH
 export PERL5LIB=`pwd`/perl:$PERL5LIB
