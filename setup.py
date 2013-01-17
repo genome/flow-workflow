2e9c338031c86a96334f0743d498e72c560f9f75
Mark Burnett <mburnett@genome.wustl.edu>
2013-01-17 09:46:21 -0600

    add nose to setup_requires

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ setup(
         name = 'flow_workflow',
         version = '0.1',
         packages = find_packages(exclude=['unit_tests']),
+        setup_requires = [
+            'nose',
+            'nosexcover',
+        ],
         install_requires = [
             'argparse',
             'redis',
