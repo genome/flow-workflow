d43050924962a89001734b185421cdc736651ec4
Mark Burnett <mburnett@genome.wustl.edu>
2013-02-27 17:38:54 -0600

    make run_workflow accept string/file handle/WF::Operation
    include a little refactoring to make this more clear

--- perl/Flow.pm
+++ perl/Flow.pm
@@ -1,14 +1,14 @@
 package Flow;
 
-use Storable qw/nfreeze thaw/;
 use Carp qw/confess/;
-use File::Slurp qw/read_file write_file/;
-use POSIX qw/WIFEXITED WEXITSTATUS/;
-use JSON;
-use Workflow;
-use File::Temp qw/tempdir/;
 use Data::Dumper;
+use File::Slurp qw/read_file/;
+use File::Temp qw/tempdir/;
+use JSON;
 use MIME::Base64;
+use POSIX qw/WIFEXITED WEXITSTATUS/;
+use Storable qw/nfreeze thaw/;
+use Workflow;
 
 use strict;
 use warnings;
@@ -91,34 +91,61 @@ sub decode_io_hash {
     };
 }
 
-
 sub run_workflow {
-    my $workflow = shift;
-    my $xml = $workflow;
+    my $wf_repr = shift;
+    my $result;
+
+    my $r = ref($wf_repr);
+    if ($r) {
+        if ($r eq 'GLOB') {
+            $result = _run_from_file($wf_repr, @_);
+        } elsif (UNIVERSAL::isa($wf_repr, 'Workflow::Operation')) {
+            $result = _run_from_object($wf_repr, @_);
+        } else {
+            die 'unrecognized reference';
+        }
+    } elsif (-f $wf_repr) {
+        $result = _run_from_file($wf_repr, @_);
+    } else {
+        $result = _run_from_string($wf_repr, @_);
+    }
+
+    return $result;
+}
+
+sub _run_from_object {
+    my $wf_operation = shift;
+
+    print "Running workflow from Workflow::Operation object\n";
+
+    my $wf_string = $wf_operation->save_to_xml();
+    return _run_from_string($wf_string, @_);
+}
+
+sub _run_from_file {
+    my $file_handle_or_name = shift;
+
+    print "Running workflow from file\n";
+
+    my $wf_string = read_file($file_handle_or_name);
+    return _run_from_string($wf_string, @_);
+}
+
+sub _run_from_string {
+    my $wf_string = shift;
+
+    my %params = @_;
+
+    print "Running workflow from string\n";
 
     my $cleanup = !exists $ENV{FLOW_WORKFLOW_NO_CLEANUP};
     my $tmpdir = tempdir(CLEANUP => $cleanup);
-    if (ref($workflow)) {
-        $xml = join("/", $tmpdir, "workflow.xml");
-        my $xml_fh = new IO::File($xml, "w");
-        $xml_fh->write($workflow->save_to_xml());
-        $xml_fh->close();
-    }
 
-    my %params = @_;
-    print "WORKFLOW PARAMS:" . Dumper(\%params);
-
-    my $params = encode_io_hash(\%params);
-
-    my $json_path = join("/", $tmpdir, "inputs.json");
+    my $xml_path = _write_xml($tmpdir, $wf_string);
+    my $inputs_path = _write_inputs($tmpdir, \%params);
     my $outputs_path = join("/", $tmpdir, "outputs.json");
-    my $json_fh = new IO::File($json_path, "w");
 
-    print "Saved xml to $xml\n";
-    my $json = new JSON->allow_nonref;
-    $json_fh->write($json->encode($params));
-    $json_fh->close();
-    my $cmd = "flow submit-workflow --xml $xml --inputs-file $json_path " .
+    my $cmd = "flow submit-workflow --xml $xml_path --inputs-file $inputs_path " .
         "--block --outputs-file $outputs_path";
     print "EXEC: $cmd\n";
 
@@ -126,16 +153,53 @@ sub run_workflow {
     if (!WIFEXITED($ret) || WEXITSTATUS($ret)) {
         confess "Workflow submission failed";
     }
+
     if (-s $outputs_path) {
         print "run_workflow got some outputs:\n";
-        my $outputs_str = read_file($outputs_path);
-        my $outputs = $json->decode($outputs_str);
-        return decode_io_hash($outputs);
+        return _read_outputs($outputs_path);
     }
     else {
         print "run_workflow got no outputs :(\n";
         return 1;
     }
+}
+
+sub _write_xml {
+    my ($tmpdir, $wf_string) = @_;
+
+    my $xml_path = join("/", $tmpdir, "workflow.xml");
+    my $xml_fh = new IO::File($xml_path, "w");
+    $xml_fh->write($wf_string);
+    $xml_fh->close();
+    print "Saved xml to $xml_path\n";
+
+    return $xml_path;
+}
+
+sub _write_inputs {
+    my ($tmpdir, $params) = @_;
+
+    print "WORKFLOW PARAMS:" . Dumper($params);
+
+    my $inputs_path = join("/", $tmpdir, "inputs.json");
+    my $encoded_params = encode_io_hash($params);
+    my $json_fh = new IO::File($inputs_path, "w");
+
+    my $json = new JSON->allow_nonref;
+    $json_fh->write($json->encode($encoded_params));
+    $json_fh->close();
+
+    return $inputs_path;
+}
+
+sub _read_outputs {
+    my $outputs_path = shift;
+
+    my $outputs_str = read_file($outputs_path);
+    my $json = new JSON->allow_nonref;
+    my $outputs = $json->decode($outputs_str);
+
+    return decode_io_hash($outputs);
 }
 
 1;
