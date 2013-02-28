a27fa4e8e82c8772c298d4d614a2cdfc59ce9d10
David Morton <davidlmorton@gmail.com>
2013-02-28 15:10:52 -0600

    update env.sh to reflect new setup

--- env.sh
+++ env.sh
@@ -1,4 +1,6 @@
-export FLOW_REDIS_URL=vmpool83
-export AMQP_URL=amqp://guest:guest@vmpool82:5672/workflow
-export PATH=`pwd`/bin:$PATH
+#export FLOW_REDIS_URL=vmpool83
+#export AMQP_URL=amqp://guest:guest@vmpool82:5672/workflow
+export PATH=`pwd`/bin:`pwd`/../flow-core/bin:$PATH
 export PERL5LIB=`pwd`/perl:$PERL5LIB
+export FLOW_CONFIG=`pwd`/../flow-site/config/production_config.yaml
+export WF_USE_FLOW=1
