f677b5385a29538f42bd88a511f134ff7f26a996
David Morton <davidlmorton@gmail.com>
2013-03-15 14:23:06 -0500

    Have historian ignore updates if workflow_plan_id < 0

--- flow_workflow/historian/service_interface.py
+++ flow_workflow/historian/service_interface.py
@@ -12,7 +12,18 @@ class WorkflowHistorianServiceInterface(object):
         self.exchange = exchange
         self.routing_key = routing_key
 
-    def update(self, net_key, operation_id, **kwargs):
-        message = UpdateMessage(net_key=net_key, operation_id=operation_id,
-                **kwargs)
-        self.broker.publish(self.exchange, self.routing_key, message)
+    def update(self, net_key, operation_id, name, workflow_plan_id, **kwargs):
+        if workflow_plan_id < 0:
+            # ignore update (don't even make message)
+            LOG.debug("Received negative workflow_plan_id:%s, "
+                    "ignoring update (net_key=%s, operation_id=%s, name=%s,"
+                    "workflow_plan_id=%s, kwargs=%s)",
+                    workflow_plan_id, net_key, peration_id, name,
+                    workflow_plan_id, kwargs)
+        else:
+            LOG.debug("Sending update (net_key=%s, operation_id=%s, name=%s,"
+                    "workflow_plan_id=%s, kwargs=%s)",
+                    net_key, peration_id, name, workflow_plan_id, kwargs)
+            message = UpdateMessage(net_key=net_key, operation_id=operation_id,
+                    **kwargs)
+            self.broker.publish(self.exchange, self.routing_key, message)
