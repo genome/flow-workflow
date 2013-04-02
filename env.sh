6f2aa484c0e6dd6af8ecf41342d1d7a6db2c2aa0
David Morton <davidlmorton@gmail.com>
2013-04-02 15:44:50 -0500

    update env.sh to new config locations

--- env.sh
+++ env.sh
@@ -2,5 +2,5 @@
 #export AMQP_URL=amqp://guest:guest@vmpool82:5672/workflow
 export PATH=`pwd`/bin:$PATH
 export PERL5LIB=`pwd`/perl:$PERL5LIB
-export FLOW_CONFIG=`pwd`/../flow-site/config/production_config.yaml
+export FLOW_CONFIG=`pwd`/../flow-site/config/production.yaml
 export WF_USE_FLOW=1
