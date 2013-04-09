e1c1da0f41857fbb4a060bc3325eb4de4911003b
Mark Burnett <mburnett@genome.wustl.edu>
2013-04-09 10:34:12 -0500

    get rid of bogus logging.NullHandler import

--- flow_workflow/__init__.py
+++ flow_workflow/__init__.py
@@ -1,13 +1,3 @@
 import logging
-import logging.handlers
 
-try:
-    NULL_HANDLER = logging.handlers.NullHandler()
-except AttributeError:
-    class NullHandler(logging.Handler):
-        def emit(self, record):
-            pass
-
-    NULL_HANDLER = NullHandler()
-
-logging.getLogger('flow_workflow').addHandler(NULL_HANDLER)
+logging.getLogger('flow_workflow').addHandler(logging.NullHandler())
