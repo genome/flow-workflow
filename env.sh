97dffafa867d646a111c534cef77a1952546060c
David Morton <davidlmorton@gmail.com>
2013-04-25 17:00:06 -0500

    Add logic to env.sh to allow sourcing from anywhere

--- env.sh
+++ env.sh
@@ -1,4 +1,17 @@
-export PATH=`pwd`/bin:$PATH
-export PERL5LIB=`pwd`/perl:$PERL5LIB
+SOURCE="${BASH_SOURCE[0]}"
+
+# resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+
+  # if $SOURCE was a relative symlink, we need to resolve it relative to the
+  # path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+export PATH=$DIR/bin:$PATH
+export PERL5LIB=$DIR/perl:$PERL5LIB
 export WF_USE_FLOW=1
-export FLOW_CONFIG_PATH=$HOME/.flow
