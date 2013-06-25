62c81729a4e8eba96b2d9a739715db0ac040749c
David Morton <davidlmorton@gmail.com>
2013-06-25 17:49:35 -0500

    Strip unneeded stuff out of GenomeModelNet

--- flow_workflow/petri_net/future_nets/model.py
+++ flow_workflow/petri_net/future_nets/model.py
@@ -1,13 +1,5 @@
-from flow.petri_net import future
 from flow_workflow.petri_net.future_nets.base import GenomeNetBase
-from flow_workflow.petri_net.actions import data
-
 
 class GenomeModelNet(GenomeNetBase):
-    def __init__(self, input_connections, **kwargs):
-        GenomeNetBase.__init__(self, **kwargs)
+    # I can't think of what needs to go here...
 
-        # XXX This may need its own special action
-        self.start_transition.action = future.FutureAction(
-                cls=data.StoreDataAction,  # XXX Should this be LoadDataAction?
-                args={'input_connections': input_connections})
