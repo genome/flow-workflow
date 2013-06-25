b979b1c08d2ff0ccef695d78a29bc3b8ae48af67
David Morton <davidlmorton@gmail.com>
2013-06-25 17:59:44 -0500

    ParallelByNet uses target_net's values (operation_id, ect.)

--- flow_workflow/petri_net/future_nets/parrallel_by.py
+++ flow_workflow/petri_net/future_nets/parrallel_by.py
@@ -5,13 +5,22 @@ from flow_workflow.petri_net.future_nets.base import GenomeNetBase
 
 class GenomeParallelByNet(GenomeNetBase):
     """
-    Make a given <target_net> run parallel on the given <target_input_name>.
-    The target_net must be a success-failure net.
+    Make a given <target_net> run parallel on the given <parallel_property>.
+    The target_net must be a GenomeNetBase net.
     """
-    def __init__(self, name, operation_id, target_net, parallel_property,
-            input_connections, output_properties, parent_operation_id=None):
+    def __init__(self, target_net, parallel_property):
+        # adopt values from the target_net
+        operation_id = target_net.operation_id
+        parent_operation_id = target_net.parent_operation_id
+        name = "%s (parallel_by harness)" % target_net.name
+        input_connections = target_net.input_connections
+        output_properties = target_net.output_properties
+
         GenomeNetBase.__init__(self, name=name, operation_id=operation_id,
                 parent_operation_id=parent_operation_id)
+
+        # add target_net to self
+        self.add_subnet(target_net)
 
         # split_transition
         split_action = FutureAction(cls=pb.ParallelBySplit,
@@ -21,8 +30,10 @@ class GenomeParallelByNet(GenomeNetBase):
         self.split_transition = self.add_basic_transition(
                 name='ParallelBy(%s) split' % operation_id,
                 action=split_action)
-        self.split_transition.add_arc_in(self.internal_start_place)
-        self.split_transition.add_arc_out(target_net.start_place)
+        self.bridge_transitions(self.internal_start_transition,
+                self.split_transition)
+        self.bridge_transitions(self.split_transition,
+                target_net.start_transition)
 
         # join_transition
         join_action = FutureAction(cls=pb.ParallelByJoin,
@@ -31,13 +42,15 @@ class GenomeParallelByNet(GenomeNetBase):
         self.join_transition = self.add_barrier_transition(
                 name='ParallelBy(%s) join' % operation_id,
                 action=join_action)
-        self.join_transition.add_arc_in(target_net.success_place)
-        self.join_transition.add_arc_out(self.internal_success_place)
+        self.bridge_transitions(target_net.success_transition,
+                self.join_transition)
+        self.join_transition.add_arc_out(self.success_place)
 
         # fail transition
-        fail_action = FutureAction(cls=pb.ParallelByFail)
-        self.fail_transition = self.add_basic_transition(
+        target_fail_action = FutureAction(cls=pb.ParallelByFail)
+        self.target_fail_transition = self.add_basic_transition(
                 name='ParallelBy(%s) fail' % operation_id,
                 action=fail_action)
-        self.fail_transition.add_arc_in(target_net.failure_place)
+        self.bridge_transitions(target_net.failure_transition,
+                self.target_fail_transition)
         self.fail_transition.add_arc_out(self.internal_failure_place)
