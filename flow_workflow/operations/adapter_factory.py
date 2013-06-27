7d08206ad2122af67250cbeb670bd57250d65503
David Morton <davidlmorton@gmail.com>
2013-06-27 13:51:26 -0500

    Moved workflow object out of adapter base and cleaned up

--- flow_workflow/operations/adapter_factory.py
+++ flow_workflow/operations/adapter_factory.py
@@ -1,30 +1,27 @@
-from flow_workflow.workflow_parts.connectors import (InputConnector,
-        OutputConnector)
-from flow_worklow.workflow_parts.converge import ConvergeOperation
-from flow_worklow.workflow_parts.block import BlockOperation
-from flow_worklow.workflow_parts.perl_action import (CommandOperation,
-        EventOperation)
-from flow_worklow.workflow_parts.model import ModelOperation
+from flow_workflow.operations.connectors.adapters import (InputConnectorAdapter,
+        OutputConnectorAdapter)
+from flow_worklow.operations.block.adapter import BlockAdapter
+from flow_worklow.operations.converge.adapter import ConvergeAdapter
+from flow_worklow.operations.model.adapter import ModelAdapter
+from flow_worklow.operations.perl_actions.adapters import (CommandAdapter,
+        EventAdapter)
 
-class WorkflowPartFactory(object):
-    _instance = None
+import logging
 
-    @classmethod
-    def get_instance(cls):
-        if cls._instance is None:
-            cls._instance = WorkflowPartFactory()
-        return cls._instance
 
+LOG = logging.getLogger(__name__)
+
+class AdapterFactory(object):
     def __init__(self):
         self.next_operation_id = 0
         self._operation_types = {
-            "InputConnector": InputConnector,
-            "OutputConnector": OutputConnector,
-            "Workflow::OperationType::Converge": ConvergeOperation,
-            "Workflow::OperationType::Block": BlockOperation,
-            "Workflow::OperationType::Command": CommandOperation,
-            "Workflow::OperationType::Event": EventOperation,
-            "Workflow::OperationType::Model": ModelOperation,
+            "InputConnector": InputConnectorAdapter,
+            "OutputConnector": OutputConnectorAdapter,
+            "Workflow::OperationType::Converge": ConvergeAdapter,
+            "Workflow::OperationType::Block": BlockAdapter,
+            "Workflow::OperationType::Command": CommandAdapter,
+            "Workflow::OperationType::Event": EventAdapter,
+            "Workflow::OperationType::Model": ModelAdapter,
         }
 
 
@@ -48,4 +45,4 @@ class WorkflowPartFactory(object):
         cls = self._operation_types[type_class]
         operation_id = self.next_operation_id
         self.next_operation_id += 1
-        return cls(part_factory=self, operation_id=operation_id, **kwargs)
+        return cls(adapter_factory=self, operation_id=operation_id, **kwargs)
