b021a4ce805899c7ac4760cda79a941de808351d
Mark Burnett <mburnett@genome.wustl.edu>
2013-07-15 15:19:55 -0500

    add base operation

---
+++ flow_workflow/operation_base.py
@@ -0,0 +1,121 @@
+from flow_workflow import io
+from flow_workflow.operations import factory
+from flow_workflow.operations import log_manager
+
+
+class Operation(object):
+    def __init__(self, net, name, operation_id, input_connections,
+            output_properties, log_dir, parent_operation_id,
+            child_operation_ids):
+        self.net = net
+
+        self.child_operation_ids = child_operation_ids
+        self.input_connections = input_connections
+        self.log_dir = log_dir
+        self.name = name
+        self.operation_id = operation_id
+        self.output_properties = output_properties
+        self.parent_operation_id = parent_operation_id
+
+
+    def save(self):
+        factory.store_operation(self.net, self)
+
+    @property
+    def as_dict(self):
+        return {
+            '_class': self.__class__.__name__,
+            'child_operation_ids': self.child_operation_ids,
+            'input_connections': self.input_connections,
+            'log_dir': self.log_dir,
+            'name': self.name,
+            'operation_id': self.operation_id,
+            'output_properties': self.output_properties,
+            'parent_operation_id': self.parent_operation_id,
+        }
+
+    def _child_id_from(self, name):
+        return self.child_operation_ids[name]
+
+    def child_named(self, name):
+        return factory.load_operation(net=self.net,
+                operation_id=self._child_id_from(name))
+
+    @property
+    def parent(self):
+        return factory.load_operation(net=self.net,
+                operation_id=self.parent_operation_id)
+
+    @property
+    def log_manager(self):
+        return log_manager.LogManager(operation_name=self.name,
+                operation_id=self.operation_id, log_dir=self.log_dir)
+
+    @property
+    def input_names(self):
+        result = []
+        for props in self.input_connections.itervalues():
+            result.extend(props.keys())
+        return result
+
+    def load_inputs(self, parallel_id):
+        return {name: self.load_input(name, parallel_id)
+                for name in self.input_names}
+
+    def load_outputs(self, parallel_id):
+        return {name: self.load_output(name, parallel_id)
+                for name in self.output_properties}
+
+    def load_input(self, name, parallel_id):
+        return io.load_input(
+                input_connections=self.input_connections,
+                net=self.net,
+                parallel_id=parallel_id,
+                property_name=name)
+
+    def load_output(self, name, parallel_id):
+        return io.load_output(
+                net=self.net,
+                operation_id=self.operation_id,
+                parallel_id=parallel_id,
+                property_name=name)
+
+    def store_outputs(self, outputs, parallel_id):
+        for name, value in outputs.iteritems():
+            self.store_output(name, value, parallel_id)
+
+    def store_output(self, name, value, parallel_id):
+        io.store_output(
+                net=self.net,
+                operation_id=self.operation_id,
+                parallel_id=parallel_id,
+                property_name=name,
+                value=value)
+
+
+class NullOperation(Operation):
+    def __init__(self):
+        pass
+
+    def save(self):
+        pass
+
+
+class ConnectorOperation(Operation):
+    def load_output(self, name, parallel_id):
+        return self.load_input(self.net, name, parallel_id)
+
+
+class ModelOperation(Operation):
+    def __init__(self, *args, **kwargs):
+        Operation.__init__(self, *args, **kwargs)
+        self._output_connector = None
+
+    def output_connector(self):
+        if self._output_connector is None:
+            self._output_connector = self.child_named('output connector')
+
+        return self._output_connector
+
+    def load_output(self, name, parallel_id):
+        self.output_connector.load_output(self.net, name, parallel_id)
---
+++ unit_tests/test_operation_base.py
@@ -0,0 +1,120 @@
+from flow_workflow import operation_base
+
+import mock
+import unittest
+
+
+class OperationTest(unittest.TestCase):
+    def setUp(self):
+        self.child_operation_ids = {
+            'foo': 12,
+            'bar': 42,
+        }
+        self.operation_id = 5
+        self.name = 'oot'
+        self.parent_operation_id = 2
+        self.output_properties = ['baz', 'buz']
+        self.input_connections = {
+            3: {
+                'in1': 'out1',
+            },
+            4: {
+                'in2': 'out2',
+            },
+        }
+
+        self.log_dir = '/exciting/log/dir'
+
+        self.net = mock.Mock()
+        self.operation = operation_base.Operation(
+                net=self.net,
+                child_operation_ids=self.child_operation_ids,
+                input_connections=self.input_connections,
+                log_dir=self.log_dir,
+                name=self.name,
+                operation_id=self.operation_id,
+                output_properties=self.output_properties,
+                parent_operation_id=self.parent_operation_id,
+        )
+
+    def test_as_dict(self):
+        self.assertEqual({
+            '_class': 'Operation',
+            'child_operation_ids': self.child_operation_ids,
+            'input_connections': self.input_connections,
+            'log_dir': self.log_dir,
+            'name': self.name,
+            'operation_id': self.operation_id,
+            'output_properties': self.output_properties,
+            'parent_operation_id': self.parent_operation_id,
+        }, self.operation.as_dict)
+
+    def test_child_id_from_exists(self):
+        self.assertEqual(12, self.operation._child_id_from('foo'))
+
+    def test_child_id_from_missing(self):
+        with self.assertRaises(KeyError):
+            self.operation._child_id_from('MISSING')
+
+
+    def test_child_named(self):
+        with mock.patch('flow_workflow.operation_base.factory') as factory:
+            child = self.operation.child_named('foo')
+            factory.load_operation.assert_called_once_with(net=self.net,
+                    operation_id=12)
+            self.assertEqual(factory.load_operation.return_value, child)
+
+    def test_parent(self):
+        with mock.patch('flow_workflow.operation_base.factory') as factory:
+            parent = self.operation.parent
+            factory.load_operation.assert_called_once_with(net=self.net,
+                    operation_id=2)
+            self.assertEqual(factory.load_operation.return_value, parent)
+
+    def test_log_manager(self):
+        self.assertEqual('/exciting/log/dir/oot.5.out',
+                self.operation.log_manager.stdout_log_path)
+
+    def test_input_names(self):
+        self.assertEqual(['in1', 'in2'], self.operation.input_names)
+
+    def test_load_inputs(self):
+        parallel_id = mock.Mock()
+        with mock.patch('flow_workflow.operation_base.io') as io:
+            inputs = self.operation.load_inputs(parallel_id)
+            self.assertItemsEqual(['in1', 'in2'], inputs.keys())
+            self.assertEqual(2, io.load_input.call_count)
+            io.load_input.assert_any_call(
+                    input_connections=self.input_connections,
+                    net=self.net, parallel_id=parallel_id, property_name='in1')
+
+    def test_load_outputs(self):
+        parallel_id = mock.Mock()
+        with mock.patch('flow_workflow.operation_base.io') as io:
+            outputs = self.operation.load_outputs(parallel_id)
+            self.assertItemsEqual(['buz', 'baz'], outputs.keys())
+            self.assertEqual(2, io.load_output.call_count)
+            io.load_output.assert_any_call(net=self.net,
+                    parallel_id=parallel_id,
+                    operation_id=self.operation_id,
+                    property_name='buz')
+
+    def test_store_outputs(self):
+        parallel_id = mock.Mock()
+        outputs = {
+            'buz': 'buzdata',
+            'baz': 'bazdata',
+        }
+        with mock.patch('flow_workflow.operation_base.io') as io:
+            self.operation.store_outputs(outputs, parallel_id)
+            self.assertEqual(2, io.store_output.call_count)
+            io.store_output.assert_any_call(
+                    net=self.net,
+                    property_name='buz',
+                    value='buzdata',
+                    parallel_id=parallel_id,
+                    operation_id=self.operation_id)
+
+
+if __name__ == "__main__":
+    unittest.main()
