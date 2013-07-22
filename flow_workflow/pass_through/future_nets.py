d9985a55a36937d4495774388f282d8afcccfdc3
Mark Burnett <mburnett@genome.wustl.edu>
2013-07-22 17:15:50 -0500

    add Historian updates to pass-through nets

--- flow_workflow/pass_through/future_nets.py
+++ flow_workflow/pass_through/future_nets.py
@@ -1,4 +1,6 @@
+from flow.petri_net.future import FutureAction
 from flow_workflow.future_nets import WorkflowNetBase
+from flow_workflow.historian.new_action import UpdateOperationStatus
 
 
 class PassThroughNet(WorkflowNetBase):
@@ -8,3 +10,8 @@ class PassThroughNet(WorkflowNetBase):
                 self.internal_start_transition,
                 self.internal_success_transition,
                 name='skipping place')
+
+        self.observe_transition(self.internal_success_transition,
+                FutureAction(UpdateOperationStatus, operation_id=operation_id,
+                    status='done', calculate_start_time=True,
+                    calculate_end_time=True))
