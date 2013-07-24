58db4f397e7e8c344e7e57333bf8a72fc4c9b76c
Mark Burnett <mburnett@genome.wustl.edu>
2013-07-24 07:54:18 -0500

    remove extra handlers from submit-workflow

--- flow_workflow/commands/submit_workflow.py
+++ flow_workflow/commands/submit_workflow.py
@@ -2,10 +2,6 @@ from flow_workflow.commands.launch_base import LaunchWorkflowCommandBase
 from flow.configuration.inject.broker import BrokerConfiguration
 from flow.configuration.inject.redis_conf import RedisConfiguration
 from flow.configuration.inject.service_locator import ServiceLocatorConfiguration
-from flow.orchestrator.handlers import PetriCreateTokenHandler
-from flow.orchestrator.handlers import PetriNotifyPlaceHandler
-from flow.orchestrator.handlers import PetriNotifyTransitionHandler
-from flow.shell_command.fork.handler import ForkShellCommandMessageHandler
 from twisted.internet import defer
 
 
@@ -19,22 +15,7 @@ class SubmitWorkflowCommand(LaunchWorkflowCommandBase):
     local_workflow = True
 
     def setup_services(self, net):
-        self.setup_shell_command_handlers()
-        self.setup_orchestrator_handlers()
-
-        self.setup_completion_handler(net)
-
-    def setup_shell_command_handlers(self):
-        self.broker.register_handler(
-                self.injector.get(ForkShellCommandMessageHandler))
-
-    def setup_orchestrator_handlers(self):
-        self.broker.register_handler(
-                self.injector.get(PetriCreateTokenHandler))
-        self.broker.register_handler(
-                self.injector.get(PetriNotifyPlaceHandler))
-        self.broker.register_handler(
-                self.injector.get(PetriNotifyTransitionHandler))
+        pass
 
     def wait_for_results(self, block):
         return defer.succeed(block)
